8f59f45baadcfd8ff27f1df754b291ecacb041dd
Temetski33 <teemu.laasio@gmail.com>
2026-02-04 07:04:37 +0200

    Abstract factory initial commit

---
+++ src/abstractfactory/components/Button.java
@@ -0,0 +1,5 @@
+package abstractfactory.components;
+
+public interface Button {
+    public void display();
+}
---
+++ src/abstractfactory/components/TextField.java
@@ -0,0 +1,5 @@
+package abstractfactory.components;
+
+public interface TextField {
+    public void display();
+}
