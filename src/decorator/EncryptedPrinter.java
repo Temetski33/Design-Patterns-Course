e5bafd75ce00e300a95fb3895b0688c4f1a3551e
Temetski33 <teemu.laasio@gmail.com>
2026-02-25 11:23:15 +0200

    Add encryption through AES algorithm

--- src/decorator/EncryptedPrinter.java
+++ src/decorator/EncryptedPrinter.java
@@ -1,13 +1,39 @@
 package decorator;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class EncryptedPrinter extends PrinterDecorator{
+
+    private static final String KEY = "HIENOTESTKEY2345"; // 16 chars
 
     public EncryptedPrinter(Printer printer) {
         super(printer);
     }
     @Override
     public void print(String message) {
-        System.out.println("Encryyyptiooon");
-        super.print(message);
+        try {
+            String encrypted = encrypt(message);
+            super.print(encrypted); }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String encrypt(String message) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encrypted = cipher.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String encrypted) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decoded = Base64.getDecoder().decode(encrypted);
+        return new String(cipher.doFinal(decoded));
     }
 }
