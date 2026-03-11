878b67d2d5759aed5f8247f014d19a67a8cd1061
Temetski33 <teemu.laasio@gmail.com>
2026-03-11 07:05:21 +0200

    Initial commit

---
+++ src/strategy/BubbleSortStrategy.java
@@ -0,0 +1,8 @@
+package strategy;
+
+public class BubbleSortStrategy implements SortingStrategy{
+    @Override
+    public int[] sort() {
+        return new int[0];
+    }
+}
---
+++ src/strategy/Demo.java
@@ -0,0 +1,7 @@
+package strategy;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello worldddd");
+    }
+}
