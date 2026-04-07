690fdab00057d44ab5c795e736b4567a33974612
Temetski33 <teemu.laasio@gmail.com>
2026-04-08 01:46:02 +0300

    Add concrete builder functions

--- src/builder/GamingComputerBuilder.java
+++ src/builder/GamingComputerBuilder.java
@@ -1,28 +1,35 @@
 package builder;
 
 public class GamingComputerBuilder implements ComputerBuilder{
+
+    private Computer computer;
+
+    public GamingComputerBuilder(){
+        this.computer = new Computer();
+    }
+
     @Override
     public void buildProcessor() {
-
+        computer.addComponent(new Component("X Gaming 5000i 8core 4GHz"));
     }
 
     @Override
     public void buildRAM() {
-
+        computer.addComponent(new Component("Ultragamer DDR5 32Gb"));
     }
 
     @Override
     public void buildGraphicsCard() {
-
+        computer.addComponent(new Component("mVideo 100Gb 7GHz"));
     }
 
     @Override
     public void buildHardDrive() {
-
+        computer.addComponent(new Component("Storemax Pro 250Tb"));
     }
 
     @Override
     public void buildOperatingSystem() {
-
+        computer.addComponent(new Component("Linux Gaming xD"));
     }
 }
--- src/builder/OfficeComputerBuilder.java
+++ src/builder/OfficeComputerBuilder.java
@@ -1,28 +1,34 @@
 package builder;
 
 public class OfficeComputerBuilder implements ComputerBuilder{
+
+    private Computer computer;
+
+    public OfficeComputerBuilder(){
+        this.computer = new Computer();
+    }
     @Override
     public void buildProcessor() {
-
+        computer.addComponent(new Component("Impel 2core 2GHz"));
     }
 
     @Override
     public void buildRAM() {
-
+        computer.addComponent(new Component("HXK DDR3 8Gb"));
     }
 
     @Override
     public void buildGraphicsCard() {
-
+        computer.addComponent(new Component("Impel Integrated HD Graph"));
     }
 
     @Override
     public void buildHardDrive() {
-
+        computer.addComponent(new Component("Storemax H 256Gb"));
     }
 
     @Override
     public void buildOperatingSystem() {
-
+        computer.addComponent(new Component("Doors 11"));
     }
 }
