77b286d521429bc9664899157325735b80b840b8
Temetski33 <teemu.laasio@gmail.com>
2026-04-08 02:17:08 +0300

    Add second computer, cleanup main

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -4,10 +4,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Computer World!");
 
+        // Make a gaming computer
         ComputerBuilder builder = new GamingComputerBuilder();
         Director director = new Director(builder);
         director.constructComputer();
-        Computer meal = builder.getComputer();
-        System.out.println(meal);
+        Computer firstComputer = builder.getComputer();
+        printComputer(firstComputer);
+
+        // Make an office computer
+        builder = new OfficeComputerBuilder();
+        director = new Director(builder);
+        director.constructComputer();
+        Computer secondComputer = builder.getComputer();
+        printComputer(secondComputer);
+    }
+
+    public static void printComputer(Computer componentsList) {
+        System.out.println();
+        System.out.println("PC configuration: ");
+        System.out.println(componentsList);
+        System.out.println();
     }
 }
