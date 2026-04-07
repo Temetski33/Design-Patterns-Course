0ce568a7312ee738862f9aa38c779507809f83a2
Temetski33 <teemu.laasio@gmail.com>
2026-04-08 01:25:26 +0300

    Add builder files

---
+++ src/builder/ComputerBuilder.java
@@ -0,0 +1,9 @@
+package builder;
+
+public interface ComputerBuilder {
+    void buildProcessor();
+    void buildRAM();
+    void buildGraphicsCard();
+    void buildHardDrive();
+    void buildOperatingSystem();
+}
